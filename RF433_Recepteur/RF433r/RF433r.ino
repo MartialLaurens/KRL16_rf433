791629088b86eedeaf5e24c2330d00fc4d8cc2c0
EmilienCAZOR <emilien.cazor@limayrac.fr>
2018-03-14 12:27:04 +0100

    pro recepteur

--- RF433_Recepteur/RF433r/RF433r.ino
+++ RF433_Recepteur/RF433r/RF433r.ino
@@ -1,48 +1,39 @@
-//send data routine
-
 // link between the computer and the SoftSerial Shield
 //at 9600 bps 8-N-1
 //Computer is connected to Hardware UART
-//SoftSerial Shield is connected to the Software UART:D2&D3
-
+//SoftSerial Shield is connected to the Software UART:D2&D3 
+ 
 #include <SoftwareSerial.h>
-
-SoftwareSerial SoftSerial(2, 3); // TX, RX
-int buffer[64];
-int count=0;
+ 
+SoftwareSerial SoftSerial(2, 3);
+unsigned char buffer[64]; // buffer array for data recieve over serial port
+int count=0;     // counter for buffer array 
 void setup()
 {
-    SoftSerial.begin(9600);               // the SoftSerial baud rate
-    Serial.begin(9600);             // the Serial port of Arduino baud rate.
-
+  SoftSerial.begin(9600);               // the SoftSerial baud rate   
+  Serial.begin(9600);             // the Serial port of Arduino baud rate.
 }
-
+ 
 void loop()
 {
-    delay(1000);
-    SoftSerial.write(0xAA);
-    SoftSerial.write(0xFA);
-    SoftSerial.write(0xE1);
-
-    if (SoftSerial.available())              // if date is coming from software serial port ==> data is coming from SoftSerial shield
+  if (SoftSerial.available())              // if date is comming from softwareserial port ==> data is comming from SoftSerial shield
+  {
+    while(SoftSerial.available())          // reading data into char array 
     {
-        while(SoftSerial.available())          // reading data into char array
-        {
-            buffer[count++]=SoftSerial.read();     // writing data into array
-            if(count == 64)break;
-        }
-        for (int i=0; i<count; i++) {
-            Serial.print(buffer[i]);            // if no data transmission ends, write buffer to hardware serial port
-        }
-        clearBufferArray();              // call clearBufferArray function to clear the stored data from the array
-        count = 0;                       // set counter of while loop to zero
+      buffer[count++]=SoftSerial.read();     // writing data into array
+      if(count == 64)break;
     }
-    if (Serial.available())            // if data is available on hardware serial port ==> data is coming from PC or notebook
+    Serial.write(buffer,count);            // if no data transmission ends, write buffer to hardware serial port
+    clearBufferArray();              // call clearBufferArray function to clear the storaged data from the array
+    count = 0;                       // set counter of while loop to zero
+  }
+  
+  if (Serial.available())            // if data is available on hardwareserial port ==> data is comming from PC or notebook
     SoftSerial.write(Serial.read());       // write it to the SoftSerial shield
-    Serial.println("...");
+    
 }
 void clearBufferArray()              // function to clear buffer array
 {
-    for (int i=0; i<count;i++)
+  for (int i=0; i<count;i++)
     { buffer[i]=NULL;}                  // clear all index of array with command NULL
 }
