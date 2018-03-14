808e200f0775ad893ef2d97b505258d2c4b8eaaf
EmilienCAZOR <emilien.cazor@limayrac.fr>
2018-03-14 11:07:15 +0100

    prog

--- RF433_Recepteur/RF433r/RF433r.ino
+++ RF433_Recepteur/RF433r/RF433r.ino
@@ -1,38 +1,48 @@
-/**
- * Exemple de code pour la bibliothèque VirtualWire – Serveur d'envoi de texte
- */
+//send data routine
 
-#include <VirtualWire.h>
+// link between the computer and the SoftSerial Shield
+//at 9600 bps 8-N-1
+//Computer is connected to Hardware UART
+//SoftSerial Shield is connected to the Software UART:D2&D3
 
-void setup() {
-  Serial.begin(9600);
+#include <SoftwareSerial.h>
 
-  // Initialisation de la bibliothèque VirtualWire
-  // Vous pouvez changez les broches RX/TX/PTT avant vw_setup() si nécessaire
-  vw_setup(2000);
-  vw_rx_start(); // On peut maintenant recevoir des messages
+SoftwareSerial SoftSerial(11, 10); // TX, RX
+int buffer[64];
+int count=0;
+void setup()
+{
+    SoftSerial.begin(9600);               // the SoftSerial baud rate
+    Serial.begin(9600);             // the Serial port of Arduino baud rate.
 
-  Serial.println("Go !"); 
 }
 
-void loop() {
-  byte message[VW_MAX_MESSAGE_LEN];
-  byte taille_message = VW_MAX_MESSAGE_LEN;
-  // N.B. La constante VW_MAX_MESSAGE_LEN est fournie par la lib VirtualWire
+void loop()
+{
+    delay(1000);
+    SoftSerial.write(0xAA);
+    SoftSerial.write(0xFA);
+    SoftSerial.write(0xE1);
 
-  /* 
-   La variable "taille_message" doit impérativement être remise à 
-   la taille du buffer avant de pouvoir recevoir un message. 
-   Le plus simple est d'utiliser une variable locale pour ne pas
-   avoir à réassigner la valeur à chaque début de loop().
-   */
-
-  // On attend de recevoir un message
-  vw_wait_rx();
-
-  if (vw_get_message(message, &taille_message)) {
-    // On copie le message, qu'il soit corrompu ou non
-
-    Serial.println((char*) message); // Affiche le message
-  }
+    if (SoftSerial.available())              // if date is coming from software serial port ==> data is coming from SoftSerial shield
+    {
+        while(SoftSerial.available())          // reading data into char array
+        {
+            buffer[count++]=SoftSerial.read();     // writing data into array
+            if(count == 64)break;
+        }
+        for (int i=0; i<count; i++) {
+            Serial.print(buffer[i],HEX);            // if no data transmission ends, write buffer to hardware serial port
+        }
+        clearBufferArray();              // call clearBufferArray function to clear the stored data from the array
+        count = 0;                       // set counter of while loop to zero
+    }
+    if (Serial.available())            // if data is available on hardware serial port ==> data is coming from PC or notebook
+    SoftSerial.write(Serial.read());       // write it to the SoftSerial shield
+    Serial.println("...");
+}
+void clearBufferArray()              // function to clear buffer array
+{
+    for (int i=0; i<count;i++)
+    { buffer[i]=NULL;}                  // clear all index of array with command NULL
 }
